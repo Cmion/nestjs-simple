b037fca9241ff6204650e0a88839e3f724248b93
cmion <akpanudosimeon@hotmail.com>
2023-06-15 20:32:48 +0100

    chore: implemented stats
    Signed-off-by: cmion <akpanudosimeon@hotmail.com>

--- apps/api-service/src/stock/service/index.ts
+++ apps/api-service/src/stock/service/index.ts
@@ -56,7 +56,40 @@ export class StockService {
   }
 
   async getStats() {
-    return [];
+    const pipeline = [
+      {
+        $group: {
+          // Group and sum up the stats (based on the count of the name)
+          _id: '$name',
+          times_requested: { $sum: 1 },
+        },
+      },
+      {
+        $match: {
+          times_requested: { $gt: 0 }, // Could be changed to 1, so it filters out those stocks requested only once.
+        },
+      },
+      {
+        $sort: {
+          times_requested: -1,
+        },
+      },
+      {
+        // Get the first 5
+        $limit: 5,
+      },
+      {
+        $project: {
+          stock: '$_id',
+          times_requested: 1,
+          _id: 0,
+        },
+      },
+    ];
+    // console.log(aggregations);
+    return this.prisma.userStockHistory.aggregateRaw({
+      pipeline,
+    });
   }
   async getHistory(userId: string) {
     return this.prisma.userStockHistory.findMany({
